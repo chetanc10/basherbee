98a3d49e78c4516f34c16c6dfd2421a3a07904d2
ChetaN KS <chetan.kumarsanga@gmail.com>
2025-07-27 10:35:51 +0530

    Update bashrc with if-file-present-source-it

--- install.sh
+++ install.sh
@@ -9,12 +9,18 @@ fi
 bee_dir="${HOME}/.basher/cellar/packages/chetanc10/basherbee"
 
 # Create ${HOME}/.basherbee_src-rc and ${HOME}/basherbee_aliases if not available
-touch ${HOME}/.basherbee_src-rc ${HOME}/.basherbee_aliases
+bee_rcf=${HOME}/.basherbee_src-rc
+bee_aliasf=${HOME}/.basherbee_aliases
+touch ${bee_rcf} ${bee_aliasf}
 
 # Ensure ${HOME}/.basherbee_src-rc is sourced in ${HOME}/.bashrc
-grep -Fq ".basherbee_src-rc" ${HOME}/.bashrc || echo ". ${HOME}/.basherbee_src-rc" >> ${HOME}/.bashrc
+if ! grep -Fq ".basherbee_src-rc" ${HOME}/.bashrc; then
+	echo "[ -f ${bee_rcf} ] && . ${bee_rcf}" >> ${HOME}/.bashrc
+fi
 
 # Ensure ${HOME}/basherbee_aliases is sourced in ${HOME}/.bashrc
-grep -Fq ".basherbee_aliases" ${HOME}/.bashrc || echo ". ${HOME}/.basherbee_aliases" >> ${HOME}/.bashrc
+if ! grep -Fq ".basherbee_aliases" ${HOME}/.bashrc; then
+	echo "[ -f ${bee_aliasf} ] && . ${bee_aliasf}" >> ${HOME}/.bashrc
+fi
 
 exit 0
