04eb653953d29563fa08386a51e9606e5040d911
ChetaN KS <chetan.kumarsanga@gmail.com>
2025-08-19 21:09:55 +0530

    Remove cd2, copy basherbee-rc contents to ~/basherbee-rc
    Signed-off-by: ChetaN KS <chetan.kumarsanga@gmail.com>

--- install.sh
+++ install.sh
@@ -8,19 +8,13 @@ fi
 
 bee_dir="${HOME}/.basher/cellar/packages/chetanc10/basherbee"
 
-# Create ${HOME}/.basherbee_src-rc and ${HOME}/basherbee_aliases if not available
-bee_rcf=${HOME}/.basherbee_src-rc
-bee_aliasf=${HOME}/.basherbee_aliases
-touch ${bee_rcf} ${bee_aliasf}
+# Create ${HOME}/.gbasherbee-rc if not available
+bee_rcf=${HOME}/.gbasherbee-rc
+touch ${bee_rcf}
 
-# Ensure ${HOME}/.basherbee_src-rc is sourced in ${HOME}/.bashrc
-if ! grep -Fq ".basherbee_src-rc" ${HOME}/.bashrc; then
+# Ensure ${HOME}/.gbasherbee-rc is sourced in ${HOME}/.bashrc
+if ! grep -Fq ".gbasherbee-rc" ${HOME}/.bashrc; then
 	echo "[ -f ${bee_rcf} ] && . ${bee_rcf}" >> ${HOME}/.bashrc
-fi
-
-# Ensure ${HOME}/basherbee_aliases is sourced in ${HOME}/.bashrc
-if ! grep -Fq ".basherbee_aliases" ${HOME}/.bashrc; then
-	echo "[ -f ${bee_aliasf} ] && . ${bee_aliasf}" >> ${HOME}/.bashrc
 fi
 
 exit 0
